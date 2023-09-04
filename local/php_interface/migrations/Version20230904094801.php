32a8c91924b786101358908b22ff06734afff248
Denisov-Sergey <sithis206@gmail.com>
2023-09-04 12:48:46 +0600

    99372 | Справочник регионов и городов для поиска объектов

---
+++ local/php_interface/migrations/Version20230904094801.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20230904094801 extends Version
+{
+    protected $description = "99372 | Поисковая выдача / Создание справочников регионов и указание координаты для региона | Справочник Городов";
+
+    protected $moduleVersion = "4.4.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+    $hlblockId = $helper->Hlblock()->saveHlblock(array (
+  'NAME' => 'Cities',
+  'TABLE_NAME' => 'cities',
+  'LANG' => 
+  array (
+    'ru' => 
+    array (
+      'NAME' => 'Города',
+    ),
+    'en' => 
+    array (
+      'NAME' => 'Города',
+    ),
+  ),
+));
+        $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_NAME',
+  'USER_TYPE_ID' => 'string',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'E',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'SIZE' => 20,
+    'ROWS' => 1,
+    'REGEXP' => '',
+    'MIN_LENGTH' => 0,
+    'MAX_LENGTH' => 0,
+    'DEFAULT_VALUE' => '',
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Наименование города',
+    'ru' => 'Наименование города',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+            $helper->Hlblock()->saveField($hlblockId, array (
+  'FIELD_NAME' => 'UF_REGION',
+  'USER_TYPE_ID' => 'hlblock',
+  'XML_ID' => '',
+  'SORT' => '100',
+  'MULTIPLE' => 'N',
+  'MANDATORY' => 'Y',
+  'SHOW_FILTER' => 'E',
+  'SHOW_IN_LIST' => 'Y',
+  'EDIT_IN_LIST' => 'Y',
+  'IS_SEARCHABLE' => 'N',
+  'SETTINGS' => 
+  array (
+    'DISPLAY' => 'LIST',
+    'LIST_HEIGHT' => 1,
+    'HLBLOCK_ID' => 'Regions',
+    'HLFIELD_ID' => 'UF_NAME',
+    'DEFAULT_VALUE' => 0,
+  ),
+  'EDIT_FORM_LABEL' => 
+  array (
+    'en' => 'Привязка к региону',
+    'ru' => 'Привязка к региону',
+  ),
+  'LIST_COLUMN_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'LIST_FILTER_LABEL' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'ERROR_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+  'HELP_MESSAGE' => 
+  array (
+    'en' => '',
+    'ru' => '',
+  ),
+));
+        }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
