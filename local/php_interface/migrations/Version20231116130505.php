6c1868d541c87c33f8ed92f69bfe5ce7588f0249
aminakin <46899180+aminakin@users.noreply.github.com>
2023-11-16 12:05:25 +0200

    102646 | Миграции

---
+++ local/php_interface/migrations/Version20231116130505.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20231116130505 extends Version
+{
+    protected $description = "102646 | Api Тревеллайн | Свойство инфоблока для массива ссылок на фото";
+
+    protected $moduleVersion = "4.4.1";
+
+    /**
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $helper = $this->getHelperManager();
+        $iblockId = $helper->Iblock()->getIblockIdIfExists('catalog', 'catalog');
+        $helper->Iblock()->saveProperty($iblockId, array (
+  'NAME' => 'Массив фото',
+  'ACTIVE' => 'Y',
+  'SORT' => '500',
+  'CODE' => 'PHOTO_ARRAY',
+  'DEFAULT_VALUE' => 
+  array (
+    'TYPE' => 'TEXT',
+    'TEXT' => '',
+  ),
+  'PROPERTY_TYPE' => 'S',
+  'ROW_COUNT' => '1',
+  'COL_COUNT' => '30',
+  'LIST_TYPE' => 'L',
+  'MULTIPLE' => 'N',
+  'XML_ID' => NULL,
+  'FILE_TYPE' => '',
+  'MULTIPLE_CNT' => '5',
+  'LINK_IBLOCK_ID' => '0',
+  'WITH_DESCRIPTION' => 'N',
+  'SEARCHABLE' => 'N',
+  'FILTRABLE' => 'N',
+  'IS_REQUIRED' => 'N',
+  'VERSION' => '2',
+  'USER_TYPE' => 'HTML',
+  'USER_TYPE_SETTINGS' => 
+  array (
+    'height' => 200,
+  ),
+  'HINT' => '',
+  'FEATURES' => 
+  array (
+    0 => 
+    array (
+      'MODULE_ID' => 'iblock',
+      'FEATURE_ID' => 'DETAIL_PAGE_SHOW',
+      'IS_ENABLED' => 'N',
+    ),
+    1 => 
+    array (
+      'MODULE_ID' => 'iblock',
+      'FEATURE_ID' => 'LIST_PAGE_SHOW',
+      'IS_ENABLED' => 'N',
+    ),
+  ),
+));
+    
+    }
+
+    public function down()
+    {
+        //your code ...
+    }
+}
