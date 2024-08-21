397e4be8e396bedb65dd6cb34b11e9d984b1eef3
denisov <denisov@addamant.ru>
2024-08-21 16:44:34 +0300

    prod

--- local/templates/main/assets/js/app.js
+++ local/templates/main/assets/js/app.js
@@ -5222,7 +5222,7 @@
             },
             {
               PATTERN:
-                /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|mil|mobi|museum|name|net|org|pro|tel|travel|ru|com)$/,
+                /^[-a-z0-9!#$%&'*+/=?^_`{|}~]+(?:.[-a-z0-9!#$%&'*+/=?^_`{|}~]+)*@(?:[a-z0-9]([-a-z0-9]{0,61}[a-z0-9])?.)*(?:aero|arpa|asia|biz|cat|com|coop|edu|gov|info|int|jobs|list|mil|mobi|museum|name|net|org|pro|tel|travel|ru|com)$/,
             }
           ),
           equality: function (
