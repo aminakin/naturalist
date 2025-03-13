fdbfae76196e5092768581e95d7a79fef84a2323
Sergey <denisov@addamant.ru>
2025-03-13 08:17:43 +0000

    Revert "Merge branch 'revert-22e9ca59' into 'main'"
    This reverts merge request !229

---
+++ local/components/addamant/admin.bnovoupdate/.description.php
@@ -0,0 +1,14 @@
+<?php
+
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true){
+    die();
+}
+
+$arComponentDescription = [
+	"NAME" => GetMessage("ADMIN_BNOVOUPDATE"),
+	"CACHE_PATH" => "Y",
+	"PATH" => [
+		"ID" => "adminbnovoupdate",
+		"NAME" =>  GetMessage("ADMIN_BNOVOUPDATE"),
+	],
+];
