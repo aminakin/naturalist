70d17073643925a37a5fbaa85527cd90f17f89ed
Alexander <shcheglakov@addamant.ru>
2025-04-02 11:46:51 +0600

    121871 | Парсинг отзывов / Корректировка верстки для вывода виджита

--- local/components/naturalist/empty/templates/object_reviews/result_modifier.php
+++ local/components/naturalist/empty/templates/object_reviews/result_modifier.php
@@ -1,4 +1,6 @@
-<?
+<?php
+use Bitrix\Highloadblock\HighloadBlockTable;
+
 $arResult = array(
     "avgRating" => $arParams['avgRating'],
     "reviewsDeclension" => $arParams['reviewsDeclension'],
@@ -13,3 +15,17 @@ $arResult = array(
     "sectionId" => $arParams['sectionId'],
     "isUserReview" => $arParams['isUserReview'],
 );
+
+
+$commonYandexReviewsClass = HighloadBlockTable::compileEntity('YandexReviews')->getDataClass();
+$commonYandexReviews = $commonYandexReviewsClass::query()
+    ->addSelect('*')
+    ->setOrder(['ID' => 'ASC'])
+    ->setFilter(['ID' => $arResult["sectionId"]])
+    ->setCacheTtl(36000000)
+    ?->fetchAll();
+
+    
+echo '<pre>';
+var_export($commonYandexReviews);
+echo '</pre>';
