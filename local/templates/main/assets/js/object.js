7e1f28fb52715a1ff54b23f686f82bc30e9df631
Alexander <shcheglakov@addamant.ru>
2025-04-09 10:48:21 +0600

    121709 | Карточка объекта / Вывод фото в галерею

--- local/templates/main/assets/js/object.js
+++ local/templates/main/assets/js/object.js
@@ -12691,7 +12691,7 @@
             spaceBetween: 0,
             speed: 250,
             effect: "slide",
-            loop: false,
+            loop: false, //$item.querySelectorAll(".swiper-slide").length > 1,
             watchOverflow: true,
             watchSlidesProgress: true,
             preloadImages: false,
