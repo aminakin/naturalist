7caa722e034a2fbcab0b28de6d6e24101d1d35cf
EmDetey <kfcrjdsqpfzw@gmail.com>
2025-05-14 13:32:27 +0600

    122545 |миграции переноса елементов

---
+++ local/php_interface/migrations/Version20250514102843.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Sprint\Migration;
+
+class Version20250514102843 extends Version
+{
+    protected $author = "admin";
+
+    protected $description   = "122545 | Правки в верстке главной и хедера";
+
+    protected $moduleVersion = "5.0.0";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Iblock()
+                      ->addElement(
+                          $item['iblock_id'],
+                          $item['fields'],
+                          $item['properties']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function down()
+    {
+    }
+}
