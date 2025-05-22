6d0ece2b18a750bbfc395203c1eff011cc6c2dea
denisov <denisov@addamant.ru>
2025-05-22 18:48:57 +0600

    dev

--- local/php_interface/classes/YandexReviews.php
+++ local/php_interface/classes/YandexReviews.php
@@ -2,11 +2,16 @@
 namespace Naturalist;
 
 use Bitrix\Highloadblock\HighloadBlockTable;
+use Bitrix\Main\Loader;
 use Naturalist\SmartWidgetsController;
 
 class YandexReviews {
     public static function getYandexReviews($sectionIds): array
     {
+
+        Loader::includeModule('highloadblock');
+        ini_set('memory_limit', '512M');
+
         $commonYandexReviewsClass = HighloadBlockTable::compileEntity('YandexReviews')->getDataClass();
         $commonYandexReviews = $commonYandexReviewsClass::query()
             ->addSelect('*')
