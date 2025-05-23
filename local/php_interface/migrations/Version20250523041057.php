0f00c4c10ad546781ccae80c865ff3178da93286
Sergey <babienko@addamant.ru>
2025-05-23 04:18:03 +0300

    123560 | правки

---
+++ local/php_interface/migrations/Version20250523041057.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Sprint\Migration;
+
+class Version20250523041057 extends Version
+{
+    protected $author = "admin";
+
+    protected $description   = "123560 | елементы слайдера в шапке на главной";
+
+    protected $moduleVersion = "5.0.0";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->IblockElementsImport()
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Iblock()
+                      ->addElement(
+                          $item['iblock_id'],
+                          $item['fields'],
+                          $item['properties']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @return bool|void
+     */
+    public function down()
+    {
+    }
+}
